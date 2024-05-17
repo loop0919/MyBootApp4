197648835158908bbe259f4ddcf5f757ab9dc66f
loop0919 <ruupu2001@gmail.com>
2024-05-17 10:54:36 +0900

    [Add] 初期コミット

---
+++ src/main/java/com/example/my_boot_app4/MyBootApp4Application.java
@@ -0,0 +1,13 @@
+package com.example.my_boot_app4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBootApp4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBootApp4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/my_boot_app4/MyBootApp4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_boot_app4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBootApp4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
